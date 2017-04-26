6a6ccd2684580605a667cabdb995d655420eda5e
hKolev <hristiqnk@gmail.com>
2017-04-26 13:03:40 +0300

    Init base project structure

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+function handleError (error) {
+  console.log(error.toString())
+  this.emit('end')
+}
+
+
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir : '.',
+            proxy: 'hkolev.int:8080'
+        }
+    })
+})
+ 
+gulp.task('sass', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .on('error', handleError)
+    .pipe(autoprefixer({
+        browsers: ['last 5 versions'], 
+    }))
+    .pipe(sourcemaps.write('/.maps'))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+})
+
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('vendors/**/*.js', browserSync.reload);
+})
---
+++ vendors/app.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+
+	$('.slider .slides').addClass("owl-carousel");
+
+ 	 $('.slider .slides').owlCarousel({
+ 	    items: 1,
+ 	    loop: true
+	});
+
+ 	//Click event for the testimonials
+ 	// var $slides = $('.slider .slides');
+
+ 	// $('.slider_prev').on('click',(e)=>{
+ 	// 	e.preventDefault();
+
+ 	// 	$slides.trigger('prev.owl.carousel');
+ 	// });
+
+ 	// $('.slider_next').on('click',(e)=>{
+ 	// 	e.preventDefault();
+
+ 	// 	$slides.trigger('next.owl.carousel');
+ 	// });
+
+  	 // Mobile navigation
+  	//  $('.nav__trigger').on('click', (e)=>{
+  	//  	$('.nav__trigger').toggleClass('active');
+
+  	//  	$('.nav').toggleClass('active');
+  	//  });
+});
